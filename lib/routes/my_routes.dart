d541303e9ab1b60fb917fe0492f69c6f916b188a
Kamola Kazakbayeva <78652168+Bahtiyerovich@users.noreply.github.com>
2022-06-17 15:54:53 +0500

    pages

--- lib/routes/my_routes.dart
+++ lib/routes/my_routes.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:telegram/view/auth/auth_view.dart';
+import 'package:telegram/view/cart/cart_view.dart';
+import 'package:telegram/view/cart/chats/chat_view.dart';
+import 'package:telegram/view/pages/info/info_view.dart';
 
 class MyRoutes {
   static final MyRoutes _instanse = MyRoutes.init();
@@ -11,6 +14,18 @@ class MyRoutes {
     switch (s.name) {
       case '/auth':
         return MaterialPageRoute(builder: (_) => const AuthView());
- }
+      case '/cart':
+        return MaterialPageRoute(builder: (_) => const CartView());
+      case '/chat':
+        return MaterialPageRoute(builder: (_) => const ChatView());
+      case '/info':
+        return MaterialPageRoute(builder: (_) => const InfoView());
+      case '/auth':
+      //   return MaterialPageRoute(builder: (_) => const AuthView());
+      // case '/auth':
+      //   return MaterialPageRoute(builder: (_) => const AuthView());
+      // case '/auth':
+      //   return MaterialPageRoute(builder: (_) => const AuthView());
+    }
   }
 }
